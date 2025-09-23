7f44bb7a091ac4d3d9f3639c527d6ca7c9870c87
Patrick Zambrano <patrickzambrano@gmail.com>
2025-09-23 17:34:43 +0800

    error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { NavProvider } from "./components/Contexts/NavLinks.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NavProvider>
-      <App />
-    </NavProvider>
+    <App />
   </StrictMode>
 );
